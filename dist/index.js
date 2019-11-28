61c7d4737f3e1c392a6af4eaf126d410129ea34c
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-28 15:18:10 +0800

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var client_1 = __importDefault(require("./utils/client"));
 var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 var threads_1 = __importDefault(require("./threads"));
 var commands_1 = __importDefault(require("./commands"));
-dotenv_1.default.config();
+var client_1 = __importDefault(require("./utils/client"));
 console.log(process.env);
 var client = new client_1.default(process.env.FB_USER || '', process.env.FB_PASS || '', threads_1.default);
+console.log("Watching for: ", threads_1.default);
 commands_1.default(client);
 client.start();
