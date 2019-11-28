e011a0b412b27e6c8f5348436ef139c34e9e1592
Alexis Munsayac <alexis@lyon.com.ph>
2019-11-28 15:18:12 +0800

    Update threads.js

--- dist/threads.js
+++ dist/threads.js
@@ -1,5 +1,10 @@
 "use strict";
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
-    '1355449081244322',
-];
+exports.default = __spreadArrays((process.env.FB_THREADS || '').split(','));
